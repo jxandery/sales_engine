bb82adaa1278c69f618f379d3533f9a4e562c9e1
Valentino Espinoza <xvalentino13@gmail.com>
2015-01-21 23:14:24 -0700

    item parser and test working

---
+++ lib/item_parser.rb
@@ -0,0 +1,16 @@
+require_relative 'item'
+require 'csv'
+
+class ItemParser
+  attr_reader :items
+
+  def initialize
+  end
+
+  def parse
+    file = CSV.open("./data/items.csv", headers: true, header_converters: :symbol)
+    @items = file.map do |row|
+      Item.new(row)
+    end
+  end
+end
